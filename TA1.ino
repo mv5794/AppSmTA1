1f3ae64b155b7a072609c62b71ff2234df925c5f
Manuel Valencia <mv5794@hotmail.com>
2020-09-15 21:23:56 -0500

    inicial

---
+++ TA1.ino
@@ -0,0 +1,47 @@
+int verde = 3, amarillo = 2, rojo = 1 , a = 10, b = 9, c = 8, d = 7, e = 6, 
+	f = 5, g = 4;
+
+void setup() {
+  pinMode(verde, OUTPUT);
+  pinMode(amarillo, OUTPUT);
+  pinMode(rojo, OUTPUT);
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(verde, HIGH); 
+delay(1000);  
+delay(1000);   
+delay(1000); 
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+delay(1000);   
+ 
+  digitalWrite(amarillo, HIGH); 
+  digitalWrite(rojo, HIGH); 
+  digitalWrite(a, HIGH); 
+  digitalWrite(g, HIGH);                   
+  digitalWrite(LED_BUILTIN, LOW);  
+                      
+}
